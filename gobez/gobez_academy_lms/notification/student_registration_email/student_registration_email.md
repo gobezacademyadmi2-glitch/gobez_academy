c98989891e005a94887bab5a8861dd4cf9e01e5d
Ahmed <hmdzabu@gmail.com>
2025-10-07 22:58:01 +0000

    sfsdfsaf

---
+++ gobez/gobez_academy_lms/notification/student_registration_email/student_registration_email.md
@@ -0,0 +1,1 @@
+<p>Registration ID {{doc.name}} {{doc.grade}} by {{doc.i_am}} for {{doc.student_name}} student email {{doc.student_email}}</p>
---
+++ gobez/gobez_academy_lms/page/gobez_home/gobez_home.js
@@ -0,0 +1,7 @@
+frappe.pages['gobez-home'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Gobez Academy Home',
+		single_column: true
+	});
+}
