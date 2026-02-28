187bc5b9e2754f90ee88ae917bfdfbb83b1a132d
hamudizz <hmdzabu@gmail.com>
2026-02-28 04:59:49 +0000

    without fixtures

---
+++ gobez/gobez_academy_lms/doctype/educational_level/educational_level.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ahmed Salim and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Educational Level", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ gobez/gobez_academy_lms/doctype/educational_level/test_educational_level.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Ahmed Salim and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEducationalLevel(FrappeTestCase):
+	pass
